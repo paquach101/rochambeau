40f74e5778238c505d86182d7f482a68288a2e15
Phat T Quach <pq03916@srcs.loc>
2018-06-01 12:16:32 -0700

    It workss!!!!!!!!!!!!!!!!

--- rochambeau.js
+++ rochambeau.js
@@ -1,8 +1,8 @@
-
 var playerChoice;
 var computerChoice;
 var choices = ["Rock", "Paper", "Scissors", "Spock", "Lizard"];
-var score = [0,0,0];
+var score = [0, 0, 0];
+var match = [0, 0];
 
 function storePlayerChoice(choice) {
     playerChoice = choice;
@@ -11,11 +11,11 @@ function storePlayerChoice(choice) {
 }
 
 function storeComputerChoice() {
-    computerChoice = Math.floor(Math.random()*5);
+    computerChoice = Math.floor(Math.random() * 5);
     console.log("Computer choice = " + computerChoice);
 }
 
-function playGame(){
+function playGame() {
     if (playerChoice == computerChoice) {
         updateScore(1);
         displayGameResult("tie")
@@ -32,12 +32,21 @@ function playGame(){
         updateScore(2);
         displayGameResult("lose")
     }
+    updateScoreBoard();
+
+    if (score[0] == 2) {
+    updateMatch(0);
+    score[0, 0, 0];
+} else if (score[2] == 2) {
+    updateMatch(1);
+    score[0, 0, 0];
 }
 
-function displayGameResult(result){
+}
+
+function displayGameResult(result) {
     var message = "Your choice was " + choices[playerChoice] + " and the computer's choice was " + choices[computerChoice] + ".";
     if (result === "win") {
-        // Display that it was a win
         document.getElementById("result").textContent = message + " YOU WIN!";
         document.getElementById("result").className = "alert alert-success";
     } else if (result === "lose") {
@@ -47,19 +56,25 @@ function displayGameResult(result){
         document.getElementById("result").textContent = message + " A tie.";
         document.getElementById("result").className = "alert alert-info";
     }
-
-    updateScoreBoard();
 }
 
-function updateScore(val){
+function updateScore(val) {
     ++score[val];
     console.log("The score is now " + score);
 }
 
-function updateScoreBoard(){
+function updateMatch(m) {
+    ++match[m];
+    console.log("The match is now " + match);
+}
+
+
+function updateScoreBoard() {
     document.getElementById("wins").textContent = score[0];
     document.getElementById("losses").textContent = score[2];
     document.getElementById("ties").textContent = score[1];
+    document.getElementById("gameWins").textContent = match[0];
+    document.getElementById("gameLosses").textContent = match[1];
 }
 
 var rockButton = document.getElementById("rock");
@@ -69,9 +84,21 @@ var spockButton = document.getElementById("spock");
 var lizardButton = document.getElementById("lizard")
 var playButton = document.getElementById("play");
 
-rockButton.addEventListener('click', () => {storePlayerChoice(0)});
-paperButton.addEventListener('click', () => {storePlayerChoice(1)});
-scissorsButton.addEventListener('click', () => {storePlayerChoice(2)});
-spockButton.addEventListener('click', () => {storePlayerChoice(3)});
-lizardButton.addEventListener('click', () => {storePlayerChoice(4)});
-playButton.addEventListener('click', () => {playGame()});
+rockButton.addEventListener('click', () => {
+    storePlayerChoice(0)
+});
+paperButton.addEventListener('click', () => {
+    storePlayerChoice(1)
+});
+scissorsButton.addEventListener('click', () => {
+    storePlayerChoice(2)
+});
+spockButton.addEventListener('click', () => {
+    storePlayerChoice(3)
+});
+lizardButton.addEventListener('click', () => {
+    storePlayerChoice(4)
+});
+playButton.addEventListener('click', () => {
+    playGame()
+});
